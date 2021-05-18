4b4366fd3ac05d384b910df52b07fe65c04663a7
Reece Russell <me@reece-russell.co.uk>
2021-05-18 14:46:59 +0100

    Added comments to time methods

--- util/time.go
+++ util/time.go
@@ -18,6 +18,7 @@ type SystemTime struct {
 	now *time.Time
 }
 
+// NewTime returns a new, unfrozen instance of SystemTime.
 func NewTime() *SystemTime {
 	if defaultTime != nil {
 		return defaultTime
@@ -28,6 +29,9 @@ func NewTime() *SystemTime {
 	}
 }
 
+// Time returns the current UTC time and is equivalent to
+// time.Now().UTC(). However, if t is frozen, the date
+// at which it was frozen will be returned.
 func (t *SystemTime) Time() time.Time {
 	t.mu.Lock()
 	defer t.mu.Unlock()
@@ -39,6 +43,8 @@ func (t *SystemTime) Time() time.Time {
 	return time.Now().UTC()
 }
 
+// Freeze sets t's time to the current UTC time. This
+// time will be returned from Time(), until Reset() is called.
 func (t *SystemTime) Freeze() {
 	t.mu.Lock()
 	defer t.mu.Unlock()
@@ -47,6 +53,7 @@ func (t *SystemTime) Freeze() {
 	t.now = &now
 }
 
+// Reset unfreezes the instance of SystemTime.
 func (t *SystemTime) Reset() {
 	t.mu.Lock()
 	defer t.mu.Unlock()
@@ -54,14 +61,20 @@ func (t *SystemTime) Reset() {
 	t.now = nil
 }
 
+// Time returns the current UTC time and is equivalent to
+// time.Now().UTC(). However, if the default SystemTime instance,
+// is frozen, the date at which it was frozen will be returned.
 func Time() time.Time {
 	return defaultTime.Time()
 }
 
+// Freeze sets default SystemTime instance to the current UTC time. This
+// time will be returned from Time(), until Reset() is called.
 func Freeze() {
 	defaultTime.Freeze()
 }
 
+// Reset unfreezes the default SystemTime.
 func Reset() {
 	defaultTime.Reset()
 }

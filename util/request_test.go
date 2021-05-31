349a0e9a574fa55bc40fafc73cb6370d664687c4
Reece Russell <me@reece-russell.co.uk>
2021-05-31 13:25:35 +0100

    Added util to ready request body

--- util/request_test.go
+++ util/request_test.go
@@ -37,3 +37,27 @@ func TestHeader_WhereHeaderIsNotPresent_ReturnsEmptyString(t *testing.T) {
 	v := Header(req, "Foo")
 	assert.Equal(t, "", v)
 }
+
+func TestBody_GivenBase64Request_UnmarshalsBody(t *testing.T) {
+	const body = "eyJmb28iOiJiYXIifQ=="
+
+	var data map[string]interface{}
+	Body(events.APIGatewayProxyRequest{
+		IsBase64Encoded: true,
+		Body:            body,
+	}, &data)
+
+	assert.Equal(t, "bar", data["foo"])
+}
+
+func TestBody_GivenPlainTextRequest_UnmarshalsBody(t *testing.T) {
+	const body = `{"foo":"bar"}`
+
+	var data map[string]interface{}
+	Body(events.APIGatewayProxyRequest{
+		IsBase64Encoded: false,
+		Body:            body,
+	}, &data)
+
+	assert.Equal(t, "bar", data["foo"])
+}

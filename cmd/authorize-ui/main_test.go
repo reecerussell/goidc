161f45f6760573f24d407358f619673f70b6ff2f
Reece Russell <me@reece-russell.co.uk>
2021-05-31 12:17:02 +0100

    Added extension to cater for undetectable content types

--- cmd/authorize-ui/main_test.go
+++ cmd/authorize-ui/main_test.go
@@ -76,6 +76,7 @@ func TestHandle_WhereFileExists_ReturnsFileAsBase64(t *testing.T) {
 	t.Run("Should Return Content Type", func(t *testing.T) {
 		assert.Contains(t, resp.Headers["Content-Type"], "text/html")
 		assert.Equal(t, strconv.Itoa(len(testFileData)), resp.Headers["Content-Length"])
+		assert.Equal(t, "public, max-age=604800, immutable", resp.Headers["Cache-Control"])
 	})
 
 	t.Run("Should Return Content", func(t *testing.T) {
@@ -141,4 +142,85 @@ func TestHandle_WhereS3ReturnsError_ReturnsInternalServerError(t *testing.T) {
 		_, ok := data["error"]
 		assert.True(t, ok)
 	})
+}
+
+func TestHandle_GivenUndetectableContentType_ReturnsFileAsBase64(t *testing.T) {
+	testFileData := []byte(`html,body{padding:0;}`)
+	testVersion := "integration-tests"
+	testFileName := "styles.css"
+	s3Key := fmt.Sprintf("%s/%s", testVersion, testFileName)
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	uploader := s3manager.NewUploader(sess)
+
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(os.Getenv("UI_BUCKET")),
+		Key:    aws.String(s3Key),
+		Body:   bytes.NewBuffer(testFileData),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	t.Cleanup(func() {
+		svc := s3.New(sess)
+		_, err := svc.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: aws.String(os.Getenv("UI_BUCKET")),
+			Key:    aws.String(s3Key),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		err = svc.WaitUntilObjectNotExists(&s3.HeadObjectInput{
+			Bucket: aws.String(os.Getenv("UI_BUCKET")),
+			Key:    aws.String(s3Key),
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	h := &Handler{sess: sess}
+	ctx := context.Background()
+	resp, err := h.Handle(ctx, events.APIGatewayProxyRequest{
+		Path: "/oauth/authorize/" + testFileName,
+		StageVariables: map[string]string{
+			"UI_BUCKET":  os.Getenv("UI_BUCKET"),
+			"UI_VERSION": testVersion,
+		},
+	})
+	assert.NoError(t, err)
+
+	t.Run("Should Return OK", func(t *testing.T) {
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("Should Return Content Type", func(t *testing.T) {
+		assert.Contains(t, resp.Headers["Content-Type"], "text/css")
+		assert.Equal(t, strconv.Itoa(len(testFileData)), resp.Headers["Content-Length"])
+	})
+
+	t.Run("Should Return Content", func(t *testing.T) {
+		assert.True(t, resp.IsBase64Encoded)
+
+		data, err := base64.StdEncoding.DecodeString(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, string(testFileData), string(data))
+	})
+}
+
+func TestGetFileTypeByExtension(t *testing.T) {
+	testData := map[string]string{
+		"test.js":        "application/javascript; charset=utf-8",
+		"styles.min.css": "text/css; charset=utf-8",
+		".my-file-type":  "text/plain; charset=utf-8",
+		"index.html":     "text/html; charset=utf-8",
+	}
+
+	for file, expected := range testData {
+		assert.Equal(t, expected, getFileTypeByExtension(file))
+	}
 }

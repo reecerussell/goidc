08d477ffab7b0bb6642dd3bb15c456b0dd9c0177
Reece Russell <me@reece-russell.co.uk>
2021-06-01 10:00:32 +0100

    Added deployment

--- scripts/deploy_ui.sh
+++ scripts/deploy_ui.sh
@@ -11,8 +11,18 @@ set -e
 echo "Env: $ENV"
 echo "Version: $VERSION"
 echo "Variable: $VAR"
+echo "Name: $NAME"
 
+echo "Updating stage..."
 aws apigateway update-stage \
     --rest-api-id "$REST_API_ID" \
     --stage-name "$ENV" \
     --patch-operations op="replace",path=/variables/"$VAR",value=$VERSION
+
+echo "Deploying..."
+aws apigateway create-deployment \
+    --rest-api-id "$REST_API_ID" \
+    --stage-name "$ENV" \
+    --description "Deployed $NAME ($VERSION)"
+
+echo "Done!"

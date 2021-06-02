ed3d567358a42eaba69e24bef6ef1939316efb04
Reece Russell <me@reece-russell.co.uk>
2021-06-02 10:39:49 +0100

    Added users proxy

--- terraform/api/users/create-user.tf
+++ terraform/api/users/create-user.tf
@@ -5,12 +5,14 @@ module "create_user" {
   http_method = "POST"
 
   api_gateway_id   = var.api_gateway_id
-  root_resource_id = var.root_resource_id
+  root_resource_id = aws_api_gateway_resource.users_proxy.id
   s3_bucket        = var.s3_bucket
   aws_region       = var.aws_region
   aws_account_id   = var.aws_account_id
 
   iam_policies = ["arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"]
+
+  depends_on = [aws_api_gateway_resource.users_proxy]
 }
 
 module "create_user_dev" {
